21e939aafe088685d27998c8aa3120924c27c3f2
Zac Morris <zac@vault.io>
2018-07-24 14:43:26 -0400

    Completely redoing the ABI stuff

--- ABI.swift
+++ ABI.swift
@@ -7,113 +7,203 @@
 
 import BigInt
 
-public struct ABIData: UnformattedDataType {
-    public static var byteCount: UnformattedDataMode {
-        return .unlimited
+public indirect enum ABIType: CustomStringConvertible {
+    case bool(value: Bool)
+    case int(size: Int, value: BigInt)
+    case uint(size: Int, value: BigUInt)
+    case string(value: String)
+    case address(value: Address)
+    case bytes(count: UnformattedDataMode, value: Data)
+    case array(count: UnformattedDataMode, type: ABIType, value: [ABIType])
+    case functionSelector(name: String, parameterTypes: [ABIType], contract: Address?)
+
+    public var description: String {
+        switch self {
+        case .bool:
+            return "bool"
+        case let .int(size, _):
+            return "int\(size)"
+        case let .uint(size, _):
+            return "uint\(size)"
+        case .string:
+            return "string"
+        case .address:
+            return "address"
+        case let .bytes(constraint, _):
+            switch constraint {
+            case .unlimited:
+                return "bytes"
+            case let .constrained(count):
+                return "bytes\(count)"
+            }
+        case let .array(constraint, type, _):
+            switch constraint {
+            case .unlimited:
+                return String(describing: type) + "[]"
+            case let .constrained(count):
+                return String(describing: type) + "[\(count)]"
+            }
+        case let .functionSelector(name, parameterTypes, _):
+            let parameterString = parameterTypes
+                .compactMap { String(describing: $0) }
+                .joined(separator: ",")
+            return "\(name)(\(parameterString))"
+        }
     }
     
-    public let data: Data
-    
-    public init(data: Data) {
-        self.data = data
+    public var isDynamic: Bool {
+        switch self {
+        case .bool:
+            return false
+        case .int:
+            return false
+        case .uint:
+            return false
+        case .string:
+            return true
+        case .address:
+            return false
+        case let .bytes(constraint, _):
+            switch constraint {
+            case .unlimited:
+                return true
+            case .constrained:
+                return false
+            }
+        case let .array(constraint, type, _):
+            switch constraint {
+            case .unlimited:
+                return true
+            case .constrained:
+                return type.isDynamic
+            }
+        case .functionSelector:
+            return false
+        }
     }
-    
-    public static func encode<T: ABIValueType>(from: T) -> ABIData {
-        return from.toABIData { ABIData(data: $0) }
+
+    public func encode() -> Data {
+        var data = Data()
+        switch self {
+        case let .bool(value):
+            data.append(Data(repeating: 0, count: 31))
+            data.append(value ? 1 : 0)
+        case let .int(_, value):
+            let magnitude = value.magnitude
+            let valueData: Data
+            if value.sign == .plus {
+                valueData = magnitude.serialize()
+            } else {
+                let serializedLength = magnitude.serialize().count
+                let max = BigUInt(1) << (serializedLength * 8)
+                valueData = (max - magnitude).serialize()
+            }
+            
+            if valueData.count > 32 {
+                fatalError()
+            }
+            
+            if value.sign == .plus {
+                data.append(Data(repeating: 0, count: 32 - valueData.count))
+            } else {
+                data.append(Data(repeating: 255, count: 32 - valueData.count))
+            }
+            data.append(valueData)
+        case let .uint(_, value):
+            let valueData = value.serialize()
+            if valueData.count > 32 {
+                fatalError()
+            }
+            
+            data.append(Data(repeating: 0, count: 32 - valueData.count))
+            data.append(valueData)
+        case let .address(value):
+            let padding = 32 - value.data.count
+            data.append(Data(repeating: 0, count: padding))
+            data.append(value.data)
+        case let .string(value):
+            guard let bytes = value.data(using: .utf8) else {
+                fatalError()
+            }
+            
+            let sizeType: ABIType = .uint(size: 256, value: BigUInt(bytes.count))
+            data.append(sizeType.encode())
+            data.append(bytes)
+            let padding = 32 - (bytes.count % 32)
+            data.append(Data(repeating: 0, count: padding))
+        case let .bytes(constraint, value):
+            switch constraint {
+            case .unlimited:
+                let sizeType: ABIType = .uint(size: 256, value: BigUInt(value.count))
+                data.append(sizeType.encode())
+            case .constrained:
+                break
+            }
+            data.append(value)
+            let padding = 32 - (value.count % 32)
+            data.append(Data(repeating: 0, count: padding))
+        case let .array(constraint, type, value):
+            switch constraint {
+            case .unlimited:
+                let sizeType: ABIType = .uint(size: 256, value: BigUInt(value.count))
+                data.append(sizeType.encode())
+            case .constrained:
+                break
+            }
+            
+            var elemDatas: [Data] = []
+            for arrayElem in value {
+                let elemData = arrayElem.encode()
+                elemDatas.append(elemData)
+            }
+            
+            if type.isDynamic {
+                var headData = Data()
+                var valueData = Data()
+                var dynamicOffset = 32 * elemDatas.count
+                for elemData in elemDatas {
+                    let sizeType: ABIType = .uint(size: 256, value: BigUInt(dynamicOffset))
+                    headData.append(sizeType.encode())
+                    dynamicOffset = dynamicOffset + elemData.count
+                    valueData.append(elemData)
+                }
+                data.append(headData)
+                data.append(valueData)
+            } else {
+                for elemData in elemDatas {
+                    data.append(elemData)
+                }
+            }
+        case let .functionSelector(_, _, contract):
+            let funcSig = String(describing: self)
+            
+            if let contract = contract {
+                data.append(contract.abiType.encode())
+            }
+            guard let asciiBytes = funcSig.data(using: .ascii) else {
+                fatalError()
+            }
+            let fullHash = asciiBytes.sha3(.keccak256)
+            data.append(fullHash[0 ..< 4])
+        }
+        
+        return data
     }
 }
 
 public protocol ABIValueType {
-    var isDynamic: Bool { get }
-    func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData
+    var abiType: ABIType { get }
 }
 
-extension Data: ABIValueType {
-    public var isDynamic: Bool {
-        if self.count > 32 {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        var data = Data()
-        if self.count > 32 {
-            let sizeData = ABIData.encode(from: self.count)
-            data.append(sizeData.data)
-        }
-        
-        data.append(self)
-        let padding = (self.count + (32 - (self.count % 32)))
-        data.append(Data(repeating: 0, count: padding))
-        return lift(data)
-    }
-}
-
-extension String: ABIValueType {
-    public var isDynamic: Bool {
-        return true
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        guard let bytes = self.data(using: .utf8) else {
-            fatalError()
-        }
-        
-        var data = Data()
-        let sizeData = ABIData.encode(from: bytes.count)
-        data.append(sizeData.data)
-        
-        data.append(bytes)
-        let padding = (bytes.count + (32 - (bytes.count % 32)))
-        data.append(Data(repeating: 0, count: padding))
-        return lift(data)
-    }
-}
-
-extension Address: ABIValueType {
-    public var isDynamic: Bool {
-        return false
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        var data = Data()
-        let padding = 32 - self.data.count
-        data.append(Data(repeating: 0, count: padding))
-        data.append(self.data)
-        return lift(data)
+extension Bool: ABIValueType {
+    public var abiType: ABIType {
+        return .bool(value: self)
     }
 }
 
 extension BigInt: ABIValueType {
-    public var isDynamic: Bool {
-        return false
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        let magnitude = self.magnitude
-        let valueData: Data
-        if self.sign == .plus {
-            valueData = magnitude.serialize()
-        } else {
-            let serializedLength = magnitude.serialize().count
-            let max = BigUInt(1) << (serializedLength * 8)
-            valueData = (max - magnitude).serialize()
-        }
-        
-        if valueData.count > 32 {
-            fatalError()
-        }
-        
-        var data = Data()
-        if self.sign == .plus {
-            data.append(Data(repeating: 0, count: 32 - valueData.count))
-        } else {
-            data.append(Data(repeating: 255, count: 32 - valueData.count))
-        }
-        data.append(valueData)
-        return lift(data)
+    public var abiType: ABIType {
+        return .int(size: 256, value: self)
     }
 }
 
@@ -122,86 +212,55 @@ extension BigUInt: ABIValueType {
         return false
     }
     
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        let valueData = self.serialize()
-        if valueData.count > 32 {
-            fatalError()
-        }
-        
-        var data = Data()
-        data.append(Data(repeating: 0, count: 32 - valueData.count))
-        data.append(valueData)
-        return lift(data)
+    public var abiType: ABIType {
+        return .uint(size: 256, value: self)
     }
 }
 
 extension Int: ABIValueType {
-    public var isDynamic: Bool {
-        return false
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        return BigInt(self).toABIData(lift: lift)
+    public var abiType: ABIType {
+        return .int(size: 64, value: BigInt(self))
     }
 }
 
 extension UInt: ABIValueType {
-    public var isDynamic: Bool {
-        return false
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        return BigUInt(self).toABIData(lift: lift)
+    public var abiType: ABIType {
+        return .uint(size: 64, value: BigUInt(self))
     }
 }
 
-extension Bool: ABIValueType {
-    public var isDynamic: Bool {
-        return false
+extension Address: ABIValueType {
+    public var abiType: ABIType {
+        return .address(value: self)
     }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        var data = Data()
-        data.append(Data(repeating: 0, count: 31))
-        data.append(self ? 1 : 0)
-        return lift(data)
+}
+
+extension String: ABIValueType {
+    public var abiType: ABIType {
+        return .string(value: self)
+    }
+}
+
+extension Data: ABIValueType {
+    public var abiType: ABIType {
+        if self.count > 32 {
+            return .bytes(count: .unlimited, value: self)
+        } else {
+            return .bytes(count: .constrained(self.count), value: self)
+        }
     }
 }
 
 extension Array: ABIValueType where Element == ABIValueType {
-    public var isDynamic: Bool {
-        return self.contains(where: { $0.isDynamic })
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        var headSize = 0
-        for subValue in self {
-            if subValue.isDynamic {
-                headSize += 32
-            } else {
-                let subData = subValue.toABIData(lift: lift).data
-                headSize += subData.count
-            }
+    public var abiType: ABIType {
+        if self.count == 0 {
+            fatalError()
         }
-        
-        var data = Data()
-        var dynamicOffset = 0
-        for subValue in self {
-            let subData = subValue.toABIData(lift: lift).data
-            if subValue.isDynamic {
-                let sizeData = ABIData.encode(from: headSize + dynamicOffset)
-                data.append(sizeData.data)
-                dynamicOffset += subData.count
-            } else {
-                data.append(subData)
-            }
+        var valueArray: [ABIType] = []
+        for elem in self {
+            valueArray.append(elem.abiType)
         }
-        
-        for subValue in self where subValue.isDynamic {
-            let subData = subValue.toABIData(lift: lift).data
-            data.append(subData)
-        }
-        return lift(data)
+        return .array(count: .unlimited, type: self[0].abiType, value: valueArray)
     }
 }
 
@@ -210,30 +269,7 @@ extension FunctionSelector: ABIValueType {
         return false
     }
     
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        let parameterString = self.parameterTypes.joined(separator: ",")
-        let asciiSig = "\(self.name)(\(parameterString))"
-        guard let asciiBytes = asciiSig.data(using: .ascii) else {
-            fatalError()
-        }
-        let fullHash = asciiBytes.sha3(.keccak256)
-        let hash = fullHash[0 ..< 4]
-        return lift(hash)
-    }
-}
-
-extension Function: ABIValueType {
-    public var isDynamic: Bool {
-        return false
-    }
-    
-    public func toABIData(lift: @escaping (Data) -> ABIData) -> ABIData {
-        var data = Data()
-        data.append(self.functionSelector.toABIData(lift: lift).data)
-        for parameter in parameters {
-            data.append(parameter.toABIData(lift: lift).data)
-        }
-        
-        return lift(data)
+    public var abiType: ABIType {
+        return .functionSelector(name: self.name, parameterTypes: self.parameterTypes, contract: self.contract)
     }
 }
--- EtherKit/Models/Function.swift
+++ EtherKit/Models/Function.swift
@@ -5,15 +5,18 @@
 //  Created by Zac Morris on 2018-07-20.
 //
 
+import BigInt
 import Foundation
 
 public struct FunctionSelector {
     public var name: String
-    public var parameterTypes: [String]
+    public var parameterTypes: [ABIType]
+    public var contract: Address?
     
-    public init(name: String, parameterTypes: [String]) {
+    public init(name: String, parameterTypes: [ABIType], contract: Address? = nil) {
         self.name = name
         self.parameterTypes = parameterTypes
+        self.contract = contract
     }
 }
 
@@ -24,5 +27,20 @@ public struct Function {
     public init(functionSelector: FunctionSelector, parameters: [ABIValueType]) {
         self.functionSelector = functionSelector
         self.parameters = parameters
+    }
+    
+    public init(name: String, parameters: [ABIValueType]) {
+        let parameterTypes: [ABIType] = parameters.compactMap { $0.abiType }
+        self.functionSelector = FunctionSelector(name: name, parameterTypes: parameterTypes)
+        self.parameters = parameters
+    }
+    
+    public func encodeToCall() -> Data {
+        var data = Data()
+        data.append(self.functionSelector.abiType.encode())
+        for parameter in parameters {
+            data.append(parameter.abiType.encode())
+        }
+        return data
     }
 }
