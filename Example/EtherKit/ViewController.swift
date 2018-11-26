c47bf15e8da1020eea6c7288d46639c955de708a
Cole Potrocky <cole@vault.io>
2018-11-26 11:55:04 -0500

    Added PasswordStorageStrategy that permits an async request to the user, for example, to get a password.

--- Example/EtherKit/ViewController.swift
+++ Example/EtherKit/ViewController.swift
@@ -14,13 +14,32 @@ import UIKit
 class ViewController: UIViewController {
   private var generatedKey: HDKey.Private! {
     didSet {
-      generatedKey.unlocked(queue: DispatchQueue.main) {
-        self.addressField.text = $0.value?.publicKey.address.description
+      generatedKey.unlocked(queue: DispatchQueue.global()) { key in
+        DispatchQueue.main.async {
+          self.addressField.text = key.value?.publicKey.address.description
+        }
       }
     }
   }
 
   private var toAddress: Address!
+  private var confirmCallback: ((String?) -> Void)?
+  private lazy var confirmViewController: UIViewController = {
+    let viewController = UIViewController()
+    let confirmButton = UIButton()
+    confirmButton.addTarget(self, action: #selector(self.onPasswordConfirm), for: .touchUpInside)
+    confirmButton.setTitle("Send Password 0000", for: .normal)
+    confirmButton.frame = CGRect(
+      x: UIScreen.main.bounds.width / 2 - 200,
+      y: UIScreen.main.bounds.height / 2 - 200,
+      width: 400,
+      height: 50
+    )
+
+    viewController.view.addSubview(confirmButton)
+
+    return viewController
+  }()
 
   @IBOutlet var addressField: UITextField!
   @IBOutlet var signedTransactionField: UITextView!
@@ -28,7 +47,13 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let walletStorage = KeychainStorageStrategy(identifier: "etherkit.example")
+    
+    let walletStorage = PasswordStorageStrategy(identifier: "etherkit.example") { finished in
+      DispatchQueue.main.async {
+        self.present(self.confirmViewController, animated: true)
+        self.confirmCallback = finished
+      }
+    }
     _ = walletStorage.delete()
     HDKey.Private.create(
       with: MnemonicStorageStrategy(walletStorage),
@@ -42,16 +67,6 @@ class ViewController: UIViewController {
       ]
     ) {
       self.generatedKey = $0.value!
-      HDKey.Private(walletStorage, network: .main, path: [
-        KeyPathNode(at: 44, hardened: true),
-        KeyPathNode(at: 60, hardened: true),
-        KeyPathNode(at: 0, hardened: true),
-        KeyPathNode(at: 1),
-      ]).unlocked { value in
-        DispatchQueue.main.async {
-          _ = value.map { key in self.toAddress = key.publicKey.address }
-        }
-      }
     }
   }
 
@@ -62,7 +77,7 @@ class ViewController: UIViewController {
 
   @IBAction func onTap(_: UIButton) {
     let fakeTransaction = SendTransaction(
-      to: toAddress,
+      to: try! Address(describing: self.addressField.text ?? ""),
       value: UInt256(45),
       gasLimit: UInt256(90000),
       gasPrice: UInt256(400_000_000_000),
@@ -83,5 +98,11 @@ class ViewController: UIViewController {
         self.signedTransactionField.text = value.value?.description ?? ""
       }
     }
+  }
+
+  @IBAction func onPasswordConfirm(_: UIButton) {
+    dismiss(animated: true)
+    confirmCallback?("0000")
+    confirmCallback = nil
   }
 }
